82665716780ac94a67bbe8570e4103b9998b1f88
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-11 23:06:14 +0300

    fetch function

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const url = "http://localhost:3000/films"
 
+const moviePlaceHolder = ()=>{
+    fetch(url)
+    .then(res =>res.json())
+    .then(content =>{
+        const firstMovie = content[0]
+
+        const filmImg = document.getElementById("poster")
+        const movieTitle = document.getElementById("filmTitle")
+        const movieDescr = document.getElementById("movieDescription")
+        const runningTime = document.getElementById("runtime")
+        const showingTime = document.getElementById("showtime")
+        const availTicket =document.getElementById("ticketsAvailable")
